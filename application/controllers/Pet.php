c3f9aed8de20676789609c2c5b32434ed246b9f3
cain123456 <598552481@qq.com>
2017-02-07 20:16:07 +0800

    Changes to be committed: 	new file:   Pet.php  Changes not staged for commit: 	deleted:    Abc.php

---
+++ application/controllers/Pet.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pet extends CI_Controller {
+	public function index()
+	{
+		//$this->load->view('welcome_message');
+		echo '首页';
+	}
+}
